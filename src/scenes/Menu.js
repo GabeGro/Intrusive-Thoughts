beacdfdb02cc24e7769debd8abfba59159161c89
Gabriel <ggroenwo@ucsc.edu>
2025-02-12 16:43:18 -0800

    changed walker texture and added tutorial text

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,10 +8,11 @@ class Menu extends Phaser.Scene {
         this.load.image('player', './assets/red-car.png')
         this.load.image('enemy', './assets/blue-car.png')
         this.load.image('enemy-blue', './assets/white-car.png')
-        this.load.image('jay-walker', './assets/jay-walker.png')
+        this.load.image('hitchhiker', './assets/hitchhiker.png')
         //this.load.image('explosion', './assets/explosion.png')
         this.load.image('restart-button', './assets/restart-button.png')
         this.load.image('menu-button', './assets/menu-button.png')
+        this.load.image('play-button', './assets/play-button.png')
         this.load.bitmapFont('jersey', './assets/Jersey/Jersey.png', './assets/Jersey/Jersey.xml')
         this.load.spritesheet('explosion', '/assets/explosion.png', {
             frameWidth: 128,
@@ -26,15 +27,20 @@ class Menu extends Phaser.Scene {
         //display menu text
         this.add.bitmapText(300, 275, 'jersey', 'Intrusive\nThoughts', 130).setOrigin(0.5, 0.5)
 
+        //menu button
+        let restartButton = this.add.image(300, 500, 'play-button').setScale(0.8).setInteractive().on('pointerdown', () => {
+            this.scene.start('playScene')
+        }).on('pointerover', () => restartButton.setTint(0xaaaaaa)).on('pointerout', () => restartButton.clearTint())
+
         //add inputs
-        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+        //this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
     }
 
     update() {
         //start game
-        if(Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
+        /*if(Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
             this.scene.start('playScene')
-        }
+        }*/
         
         //update highway
         this.highway.tilePositionY -= 2.5
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,9 @@ class Play extends Phaser.Scene {
         //game over flag
         this.gameOver = false
 
+        //game start flag
+        this.gameStart = false
+
         //game speed
         this.gameSpeed = 1
 
@@ -23,16 +26,81 @@ class Play extends Phaser.Scene {
         
         this.highway = this.add.tileSprite(0, 0, 600, 800, 'highway').setOrigin(0, 0)
 
-        
         //add player character
         this.playerCar = new Player(this, 362, 700, 'player').setScale(0.5)
 
-        //add enemy cars
-        this.enemyCar01 = new Enemy(this, 240, -50, 'enemy', 'left').setScale(0.5).setOrigin(0.5, 0)
-        this.enemyCar02 = new Enemy(this, 362, -50, 'enemy-blue', 'right').setScale(0.5).setOrigin(0.5, 0)
+        //add tutorial text
+        this.tutorialText1 = this.add.bitmapText(300, 250, 'jersey', 'Move With Arrows', 70).setOrigin(0.5, 0.5)
+        this.tutorialText2 = this.add.bitmapText(300, 350, 'jersey', 'Hit Hitchhikers For Points', 60).setOrigin(0.5, 0.5)
 
-        //add jay walker
-        this.walker01 = new Walker(this, 410 , -50, 'jay-walker', 10).setScale(0.017).setOrigin(0, 0.5)
+        this.time.delayedCall(4000, () => {
+            this.tutorialText1.destroy()
+            this.tutorialText2.destroy()
+            
+            //add enemy cars
+            this.enemyCar01 = new Enemy(this, 240, -150, 'enemy', 'left').setScale(0.5).setOrigin(0.5, 0)
+            this.enemyCar02 = new Enemy(this, 362, -150, 'enemy-blue', 'right').setScale(0.5).setOrigin(0.5, 0)
+
+            //add jay walker
+            this.walker01 = new Walker(this, 410 , -100, 'hitchhiker', 10).setScale(1).setOrigin(0, 0.5)
+            this.gameStart = true
+
+            //player-enemy collision
+            this.physics.add.collider(this.playerCar, this.enemyCar01, (player, enemy) => {
+                //run explosion
+                this.explosion = this.add.sprite(player.x, player.y - 150, 'explosion').setScale(3)
+                this.explosion.play('explosion')
+                this.gameOver = true
+                //delete sprites
+                player.setVisible(false)
+                enemy.setVisible(false)
+                this.gameOver = true
+                //game over text
+                this.gameOverBG = this.add.graphics()
+                this.gameOverBG.fillStyle(0x000000, 0.5)
+                this.gameOverBG.fillRect(40, 200, 520, 100)
+
+                this.gameOverText = this.add.bitmapText(300, 250, 'jersey', 'GAME OVER', 130).setOrigin(0.5, 0.5)
+
+                //restart button
+                let restartButton = this.add.image(225, 350, 'restart-button').setScale(0.5).setInteractive().on('pointerdown', () => {
+                    this.scene.restart()
+                }).on('pointerover', () => restartButton.setTint(0xaaaaaa)).on('pointerout', () => restartButton.clearTint())
+                //menu button
+                let menuButton = this.add.image(400, 350, 'menu-button').setScale(0.5).setInteractive().on('pointerdown', () => {
+                    this.scene.start('menuScene')
+                }).on('pointerover', () => menuButton.setTint(0xaaaaaa)).on('pointerout', () => menuButton.clearTint())
+            })
+            this.physics.add.collider(this.playerCar, this.enemyCar02, (player, enemy) => {
+                //run explosion
+                this.explosion = this.add.sprite(player.x, player.y - 150, 'explosion').setScale(3)
+                this.explosion.play('explosion')
+                this.gameOver = true
+                //delete sprites
+                player.setVisible(false)
+                enemy.setVisible(false)
+                //game over text
+                this.gameOverBG = this.add.graphics()
+                this.gameOverBG.fillStyle(0x000000, 0.5)
+                this.gameOverBG.fillRect(40, 200, 520, 100)
+
+                this.gameOverText = this.add.bitmapText(300, 250, 'jersey', 'GAME OVER', 130).setOrigin(0.5, 0.5)
+
+                //restart button
+                let restartButton = this.add.image(225, 350, 'restart-button').setScale(0.5).setInteractive().on('pointerdown', () => {
+                    this.scene.restart()
+                }).on('pointerover', () => restartButton.setTint(0xaaaaaa)).on('pointerout', () => restartButton.clearTint())
+                //menu button
+                let menuButton = this.add.image(400, 350, 'menu-button').setScale(0.5).setInteractive().on('pointerdown', () => {
+                    this.scene.start('menuScene')
+                }).on('pointerover', () => menuButton.setTint(0xaaaaaa)).on('pointerout', () => menuButton.clearTint())
+            })
+            //player-walker collision
+            this.physics.add.collider(this.playerCar, this.walker01, () => {
+                this.walker01.reset()
+                this.score += this.walker01.points
+            })
+        }, [], this)
 
         //player input
         this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
@@ -40,61 +108,6 @@ class Play extends Phaser.Scene {
         this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
         this.downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
 
-        //player-enemy collision
-        this.physics.add.collider(this.playerCar, this.enemyCar01, (player, enemy) => {
-            //run explosion
-            this.explosion = this.add.sprite(player.x, player.y - 150, 'explosion').setScale(3)
-            this.explosion.play('explosion')
-            this.gameOver = true
-            //delete sprites
-            player.setVisible(false)
-            enemy.setVisible(false)
-            this.gameOver = true
-            //game over text
-            this.gameOverBG = this.add.graphics()
-            this.gameOverBG.fillStyle(0x000000, 0.5)
-            this.gameOverBG.fillRect(40, 200, 520, 100)
-
-            this.gameOverText = this.add.bitmapText(300, 250, 'jersey', 'GAME OVER', 130).setOrigin(0.5, 0.5)
-
-            //restart button
-            let restartButton = this.add.image(225, 350, 'restart-button').setScale(0.5).setInteractive().on('pointerdown', () => {
-                this.scene.restart()
-            }).on('pointerover', () => restartButton.setTint(0xaaaaaa)).on('pointerout', () => restartButton.clearTint())
-            //menu button
-            let menuButton = this.add.image(400, 350, 'menu-button').setScale(0.5).setInteractive().on('pointerdown', () => {
-                this.scene.start('menuScene')
-            }).on('pointerover', () => menuButton.setTint(0xaaaaaa)).on('pointerout', () => menuButton.clearTint())
-        })
-        this.physics.add.collider(this.playerCar, this.enemyCar02, (player, enemy) => {
-            //run explosion
-            this.explosion = this.add.sprite(player.x, player.y - 150, 'explosion').setScale(3)
-            this.explosion.play('explosion')
-            this.gameOver = true
-            //delete sprites
-            player.setVisible(false)
-            enemy.setVisible(false)
-            //game over text
-            this.gameOverBG = this.add.graphics()
-            this.gameOverBG.fillStyle(0x000000, 0.5)
-            this.gameOverBG.fillRect(40, 200, 520, 100)
-
-            this.gameOverText = this.add.bitmapText(300, 250, 'jersey', 'GAME OVER', 130).setOrigin(0.5, 0.5)
-
-            //restart button
-            let restartButton = this.add.image(225, 350, 'restart-button').setScale(0.5).setInteractive().on('pointerdown', () => {
-                this.scene.restart()
-            }).on('pointerover', () => restartButton.setTint(0xaaaaaa)).on('pointerout', () => restartButton.clearTint())
-            //menu button
-            let menuButton = this.add.image(400, 350, 'menu-button').setScale(0.5).setInteractive().on('pointerdown', () => {
-                this.scene.start('menuScene')
-            }).on('pointerover', () => menuButton.setTint(0xaaaaaa)).on('pointerout', () => menuButton.clearTint())
-        })
-        //player-walker collision
-        this.physics.add.collider(this.playerCar, this.walker01, () => {
-            this.walker01.reset()
-            this.score += this.walker01.points
-        })
         //timer for speed
         this.timer = this.time.addEvent({
             delay: 5000,
@@ -114,15 +127,19 @@ class Play extends Phaser.Scene {
     }
 
     update() {
-        //update all movements
-        if(!this.gameOver) {
+        //update car and highway movements
+        if (!this.gameOver) {
+            this.highwayScroll()
             this.playerCar.update()
+        }
+        
+        //update enemy and walker movements
+        if(!this.gameOver && this.gameStart) {
             this.walker01.update(this.gameSpeed)
             this.enemyCar01.update(this.gameSpeed)
             this.enemyCar02.update(this.gameSpeed)
-            this.highwayScroll()
         }
-        if(this.gameOver) {
+        if(this.gameOver ) {
             this.walker01.body.setVelocityY(0)
             this.enemyCar01.body.setVelocityY(0)
             this.enemyCar01.body.setVelocityX(0)
@@ -134,14 +151,16 @@ class Play extends Phaser.Scene {
         //update scoreboards
         this.scoreboard.setText('Score: ' + this.score)
         //handle enemy respawn
-        if (this.enemyCar01.y > 800) {
-            this.enemyCar01.reset() 
-        } else if (this.enemyCar02.y > 800) {
-            this.enemyCar02.reset() 
-        }
-        //walker respawn
-        if (this.walker01.y > 800) {
-            this.walker01.reset()
+        if (this.gameStart) {
+            if (this.enemyCar01.y > 800) {
+                this.enemyCar01.reset() 
+            } else if (this.enemyCar02.y > 800) {
+                this.enemyCar02.reset() 
+            }
+            //walker respawn
+            if (this.walker01.y > 800) {
+                this.walker01.reset()
+            }
         }
         //console.log(`${this.playerCar.x}`)
     }
